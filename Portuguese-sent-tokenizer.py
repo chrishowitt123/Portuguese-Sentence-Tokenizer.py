19a8c7017e9bb666b1f8c2bc08898d9b5514eb2a
Chris Howitt <62956122+chrishowitt123@users.noreply.github.com>
2021-06-26 17:40:46 +0100

    Create Portuguese-sent-tokenizer.py

---
+++ Portuguese-sent-tokenizer.py
@@ -0,0 +1,11 @@
+import nltk
+nltk.download('machado')
+
+file = r"C:\Users\chris\Documents\Transgola\Clients\PROJECTS\2021\396200521_TM_ON\Orignal\Relatório e Contas 2020_2370421_VF.txt"
+
+sent_tokenizer=nltk.data.load('tokenizers/punkt/portuguese.pickle')
+raw_text = open(file, 'r').read().replace("e.g.", "e.g ")
+sentences = sent_tokenizer.tokenize(raw_text)
+for sent in sentences:
+    print(sent)
+    print('\n')
