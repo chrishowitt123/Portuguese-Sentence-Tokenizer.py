2af0ad76ee790dfbe51e656e8d3f180976a81cdf
Chris Howitt <62956122+chrishowitt123@users.noreply.github.com>
2021-06-26 18:48:35 +0100

    Update Portuguese-sent-tokenizer.py

--- Portuguese-sent-tokenizer.py
+++ Portuguese-sent-tokenizer.py
@@ -1,11 +1,26 @@
 import nltk
+import re
 nltk.download('machado')
+sent_tokenizer=nltk.data.load('tokenizers/punkt/portuguese.pickle')
+
+def replace_all(text, dic):
+    for i, j in dic.items():
+        text = text.replace(i, j)
+    return text
 
 file = r"C:\Users\chris\Documents\Transgola\Clients\PROJECTS\2021\396200521_TM_ON\Orignal\Relatório e Contas 2020_2370421_VF.txt"
+raw_text = open(file, 'r').read()
 
-sent_tokenizer=nltk.data.load('tokenizers/punkt/portuguese.pickle')
-raw_text = open(file, 'r').read().replace("e.g.", "por exemplo").replace("ex.", "por exemplo").replace("* ", "")
+replace_dict = {'e.g.': 'por exemplo',
+                'ex.' : 'por exemplo',
+                '* '  : '', 
+                '\t'  : '',
+                '\n'  : '. '}
+
+raw_text = replace_all(raw_text, replace_dict)
+
 sentences = sent_tokenizer.tokenize(raw_text)
+
 for sent in sentences:
     print(sent)
     print('\n')
