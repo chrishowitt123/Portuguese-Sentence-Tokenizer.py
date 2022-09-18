3b15b376857a1ee26b25532e1ea07195c0eb03c5
Chris Howitt <62956122+chrishowitt123@users.noreply.github.com>
2022-09-18 11:31:37 +0100

    Update Portuguese-sent-tokenizer.py

--- Portuguese-sent-tokenizer.py
+++ Portuguese-sent-tokenizer.py
@@ -5,16 +5,25 @@ import os
 nltk.download('machado')
 sent_tokenizer=nltk.data.load('tokenizers/punkt/portuguese.pickle')
 
-os.chdir(r'C:\Users\chris\Documents\Transgola\Clients\PROJECTS\2021\405010721_TM_NCR\Translation\Python')
+"""
+A program that cleans and tokenizes Portuguese text.
 
+"""
+
+os.chdir(r'')
+
+# function that uses a dictionary to replaces items within a body of text
 def replace_all(text, dic):
     for i, j in dic.items():
         text = text.replace(i, j)
     return text
 
 file = r"ORIGNAL.docx"
+
+# define text object
 text = docx2txt.process(file)
 
+# define before and after pairs
 replace_dict = {'e.g.': 'por exemplo',
                 'ex.' : 'por exemplo',
                 'Exmos.' : 'Exmos',
@@ -22,18 +31,25 @@ replace_dict = {'e.g.': 'por exemplo',
                 '\t'  : '',
                 'Kz.' : 'Kz'}
 
+# clean text using function
 text = replace_all(text, replace_dict)
+
+# prepare text for splitting into sentences
 text = re.sub(r'(\n+)',  '\n', text).replace('..', '.').replace(';.', '.').replace(';.', '.')
 
+# split text into sentences
 sentences = sent_tokenizer.tokenize(text)
 sentences = [s.split('\n') for s in sentences]
 sentences = sum(sentences, [])
 
+# print results
 for sent in sentences:
     print(sent)
     print('\n' * 2)
     
 textfile = open(f"SPLITS_{file.replace('.docx', '.txt')}", "w")
+
+# write to text file
 for sent in sentences:
     textfile.write(sent + "\n" * 2)
 textfile.close()
